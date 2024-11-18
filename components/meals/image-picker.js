9841e244932422e27be66d0c90870097b5d640a9
anouar garbaa <g.anouar@yahoo.com>
2024-11-18 14:41:16 +0100

    adding an image preview to the picker

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,19 +1,50 @@
-'use client';
+"use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 
 export default function ImagePicker({ label, name }) {
-const imageInput = useRef();
+  const [pickedImage, setPickedImage] = useState();
+
+  const imageInput = useRef();
 
   function handlePickClick() {
-    imageInput.current.click()
+    imageInput.current.click();
+  }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    //in order to show it as a preview here, i want to convert it into a data URL , which is a value that can be used as an input (a source) for an image element
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
   }
 
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user."
+              fill
+            />
+          )}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -21,6 +52,7 @@ const imageInput = useRef();
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInput}
+          onChange={handleImageChange}
         />
         <button
           className={classes.button}
