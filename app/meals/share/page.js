2291d5fdc41343c953aa5751c7567794712f8c5a
anouar garbaa <g.anouar@yahoo.com>
2024-11-18 15:26:53 +0100

    using Server Action to handle form submission

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -2,6 +2,22 @@ import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 
 export default function ShareMealPage() {
+async function shareMeal(formData) {
+  'use server'; //this creates a so-called Server Action, which is a function that's guaranteed to execute on the server, and only there.
+
+  const meal = {
+    title: formData.get('title'),
+    summary : formData.get('summary'),
+    instructions : formData.get('instructions'),
+    image : formData.get('image'),
+    creator : formData.get('name'),
+    creator_email : formData.get('email'),
+  }
+
+  
+}
+
+
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +27,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +55,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-         <ImagePicker />
+         <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
